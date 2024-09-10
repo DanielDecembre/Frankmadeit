82baec06bc52db9dcaea49c45651f69840667c48
DanielDecembre <96702604+DanielDecembre@users.noreply.github.com>
2024-09-10 03:58:03 -0400

    video edit

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 
-import "./page.css" ;
+import "./page.css";
 import Nav from './Components/Nav/nav';
 
 export default function Home() {
@@ -7,8 +7,9 @@ export default function Home() {
     <div className="main-container">
       <Nav />
       <div className="video-container">
-        <video className="compvid" autoPlay muted loop>
+        <video className="compvid" autoPlay muted loop playsInline>
           <source src="/LANDSCAPE.mp4" type="video/mp4" />
+          <source src="/LANDSCAPE.webm" type="video/webm" />
           Your browser does not support this video tag.
         </video>
       </div>
