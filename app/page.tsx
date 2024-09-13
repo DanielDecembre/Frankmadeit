7e3c169a00b20eae632f13e73710810526944832
DanielDecembre <96702604+DanielDecembre@users.noreply.github.com>
2024-09-13 19:58:11 -0400

    differentm

--- app/page.tsx
+++ app/page.tsx
@@ -17,13 +17,13 @@ export default function Home() {
       <Nav />
       <div className="video-container">
         <div className="video-wrapper">
-        <iframe
-  src="https://www.youtube.com/embed/IWSNxeRBzu4?autoplay=1&mute=1&controls=0&rel=0&modestbranding=1&loop=1&playlist=IWSNxeRBzu4"
-  title="YouTube video player"
-  frameBorder="0"
-  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-  allowFullScreen={true}
-></iframe>
+        <iframe 
+    src="https://www.youtube.com/embed/FWMvDwjb0Cg?si=xeZqoyl4G9iQz5Ps&autoplay=1&mute=1&loop=1&controls=0&start=1&playlist=FWMvDwjb0Cg"
+    title="YouTube video player" 
+    frameBorder="0" 
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+    allowFullScreen
+  ></iframe>
           {isMuted && (
             <button className="unmute-button" onClick={toggleMute}>
               Unmute
