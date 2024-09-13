32227ac3ab5c88c7cf479b18994c4b15b060dc8d
DanielDecembre <96702604+DanielDecembre@users.noreply.github.com>
2024-09-13 17:09:22 -0400

    new video

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,18 @@ import Nav from './Components/Nav/nav';
 
 export default function Home() {
   return (
-    <div className="main-container">
-      <Nav />
-      <div className="video-container">
-        <video className="compvid" autoPlay muted loop playsInline>
-          <source src="/thereonce.mp4" type="video/mp4" />
-          Your browser does not support this video tag.
-        </video>
-      </div>
-    </div>
+  <div className="main-container">
+  <Nav />
+  <div className="video-container">
+    <iframe
+      className="compvid"
+      src="https://www.youtube.com/embed/IWSNxeRBzu4"
+      title="YouTube video player"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+    ></iframe>
+  </div>
+</div>
   );
 }
